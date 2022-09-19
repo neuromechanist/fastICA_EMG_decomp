bd745d95dfeba18411057aef31021830fe83b4d8
Seyed Yahya Shirazi <shirazi@ieee.org>
2022-09-18 21:40:03 -0400

    Update main_emg_decomposition.m
    Changed paths and the for loops for better debugging of the code

--- main_emg_decomposition.m
+++ main_emg_decomposition.m
@@ -1,10 +1,10 @@
-clear all;
+clearvars;
 close all;
 
 subject={'01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20'};
 
-path_data='D:\jiangxinyu\Open Access HDsEMG DataSet Analysis\hyser_dataset'; % change path_data to the location of the dataset
-path_results='..\results\decomposition results\1DoF'; % change path_results to the location to save decomposition results
+path_data='/Volumes/Yahya/Datasets/hyser/'; % change path_data to the location of the dataset
+path_results=[path_data '/results/decomposition results/1DoF']; % change path_results to the location to save decomposition results
 mkdir(path_results);
           
 fs_emg=2048;
@@ -13,14 +13,14 @@ M = 300; % FastICA iteration number
 
 threshold = 0.6; % shreshold to select motor units
 
-parfor i=1:20
+for i=1:2
     for session=1:2%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         
         emg_finger_1dof=load_1dof(path_data,subject{1,i},session,'raw');
         for u=1:5 %%%%%%%%%%%%%%%%%% u=1:5
             for v=1:3
                 for muscle_idx=1:2
-                    i
+                    i %#ok<*NOPTS> 
                     session
                     u
                     v
